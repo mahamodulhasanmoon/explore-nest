88a9c93d321f5907fa39d9336df4c4b7d56f0010
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-09-11 00:03:20 +0600

    basic 2 routes done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,12 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('bye')
+  getBye(): string {
+    return this.appService.getBye();
   }
 }
